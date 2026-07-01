036a06e9e052ad7afab62e7df724807996a27529
Sandhya <ragirisandhya24@gmail.com>
2026-07-01 20:30:23 +0530

    added counter

--- index.js
+++ index.js
@@ -1,29 +1,9 @@
-const button = document.querySelector("#cookiebtn");
-const Counter = document.querySelector("#cookiecounter");
-const counterSecs = document.querySelector("#counterSec");
-let countCookie = 0;
-let cookieSeconds = 0;
-Counter.innerText = countCookie;
-button.addEventListener("click", function(){
-  countCookie++;
-  Counter.innerText = countCookie;
-});
+const cookiebtn = document.querySelector("#cookiebtn");
+const counterDisplay = document.querySelector("#counter");
 
-//perseconds
-setInterval(()=>{
-  cookieSeconds ++;
-  Counter.innerText = cookieSeconds;
-}, 1000);
 
-button.addEventListener("click", cookieScale);
-function cookieScale(){
-   button.addEventListener("mousedown", () => {
-  button.style.transform = "scale(0.9)";
-  button.style.transition = "0.3";
+let counter = 0;
+cookiebtn.addEventListener("click", function(){
+  counter ++;
+  counterDisplay.innerText = counter;
 });
-
-button.addEventListener("mouseup", () => {
-  button.style.transform = "scale(1)";
-   button.style.transition = "0.3";
-});
-}
