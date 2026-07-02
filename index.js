bb7070a2d91cd15f7973cd746a1aaa320f342465
Sandhya <ragirisandhya24@gmail.com>
2026-07-02 20:18:27 +0530

    added baker button

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const cookiebtn = document.querySelector("#cookiebtn");
 const counterDisplay = document.querySelector("#counter");
 const cpsDisplay = document.querySelector("#counterSec");
 const grandmotherBtn = document.querySelector("#item1");
+const bakerBtn = document.querySelector("#item2");
 
 
 let counter = 0;
@@ -23,6 +24,23 @@ grandmotherBtn.addEventListener("click", ()=>{
     alert("You can't afford cookies");
   }
 });
+
+//BAKER button
+
+bakerBtn.addEventListener("click", ()=>{
+  if(counter >=500){
+    counter -=500;
+    counterSeconds++;
+    cpsDisplay.innerText = `${counterSeconds}/S`;
+  }
+
+  else {
+    alert("You can't afford baker");
+  }
+});
+
+
+
 setInterval(()=>{
    counter = counter + counterSeconds;
   counterDisplay.innerText = counter;
